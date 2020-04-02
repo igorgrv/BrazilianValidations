ab8666116e314235d2d8717844b08b09540e9477
Igor <igorgv@hotmail.com>
2020-04-02 11:57:53 -0300

    Formatting Dates

---
+++ brazilValidation/src/main/java/com/brazil/validation/documents/Dates.java
@@ -0,0 +1,31 @@
+package com.brazil.validation.documents;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
+public class Dates {
+
+	public static void main(String[] args) {
+		LocalDate today = LocalDate.now();
+		System.out.println(today);
+		//2020-04-02
+
+		DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		System.out.println(today.format(dateFormatter));
+		//02/04/2020
+		
+		LocalDateTime now = LocalDateTime.now();
+		System.out.println(now);
+		//2020-04-02T11:53:41.402882600
+		
+		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy hh:MM");
+		System.out.println(now.format(dateTimeFormatter));
+		//02/04/2020 11:04
+		
+		DateTimeFormatter dateTimeFormatterLong = DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG);
+		System.out.println(now.format(dateTimeFormatterLong));
+		//2 de abril de 2020
+	}
+}
